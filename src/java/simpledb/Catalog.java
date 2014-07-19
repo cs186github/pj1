82ff8c8da69f9d2ede963a8784c711197ddd74de
darkangle <cs186github@yeah.net>
2014-07-19 23:57:59 +0800

    System catalog is completed. Waiting for test and bug fix.

--- src/java/simpledb/Catalog.java
+++ src/java/simpledb/Catalog.java
@@ -23,17 +23,100 @@ public class Catalog {
    public abstract CatalogEntry{
      
      /**
-      * Specify a unique entry in the system catalog. 
+      * Specify a unique entry in the system catalog. For each entry in a catalog
+      * this item should be unique. 
       */
      private int entryID; 
-     
+     /**
+      * Specify a name for this catalog entry.
+      */
+     private String entryName;
+
+     /**
+      * Return the string format of this entry.
+      */ 
      public String toString();
    }
    /**
     * Instance of CatalogItem that stores the information about a heap file.
+    *
+    * Inherited fields:
+    * 		private int entryID
+    *		private String entryName 
+    * 
+    * Also this catalog entry maintains such specific information:
+    * 		(1)The primary key of this table
+    *		(2)The corresponding tuple desicription of this table, which 
+    *		   contains the data schema.
+    *		(3)The number of pages that this table has occupied.
+    *		(4)The header of the page, which records whether a slot a occupied 
+    *		   or not.
+    *		(5)The number of slots of a heap page.
+    *
     */ 
-   public class CatalogHeap implements CatalogEntry{{
+   public class CatalogHeap implements CatalogEntry{
+     /**
+      * The primary key of this relation.
+      */
+     private String pkey;
+     /**
+      * References the TupleDesc of this relation.
+      */
+     private TupleDesc tds;
+     /**
+      * The number of pages this table has.
+      */
+     private int numPage;
+     /**
+      * The length of header part in a HeapPage, Caculated by bytes.
+      */
+     private int headerLen;
+     /**
+      * The number of slots in a HeapPage.
+      */
+     private int numSlots;
+     /**
+      * The DbFile instance of this heap file.
+      */
+     private DbFile dbf;
+
+     ///**
+     // * The location of this table on the system disk.
+     // */
+     //private String tableLoc;
+
+     /**
+      * Constructs a catalog entry storing information about a heap table.
+      * 
+      * @param file the DbFile instance of this heap fle. 
+      * @param tname the name of this relation.
+      * @param pkey specifies the primary key of this relation.
+      * @param tds specifies ths skema of this relation. 
+      */
+      public CatalogHeap(DbFile file, String tname, String pkey, TupleDesc tds){
+        this.entryID = file.getId();
+        this.entryName = tname;
+        this.numPage = 0;
+        this.pkey = pkey;
+        this.tds = tds;
+        this.dbf = file; 
+        if(tds != null){
+          this.numSlots = (BufferPool.PAGE_SIZE * 8)/(tds.getSize() * 8 + 1);
+          this.headerLen = (int) Math.ceil(((double) this.numSlots)/8); 
+        }
+      } 
+
+      /**
+       * Return the String representation of this entry item.
+       */
+      public String toString(){
+        return "This feature has not been implemented yet.\n"
+      }
+     
+    
    }
+  
+    HashTable<Integer, CatalogEntry> catalog; 
 
     /**
      * Constructor.
@@ -41,6 +124,7 @@ public class Catalog {
      */
     public Catalog() {
         // some code goes here
+        catalog = new HashTable<Integer, CatalogEntry>();
     }
 
     /**
@@ -49,11 +133,19 @@ public class Catalog {
      * @param file the contents of the table to add;  file.getId() is the identfier of
      *    this file/tupledesc param for the calls getTupleDesc and getFile
      * @param name the name of the table -- may be an empty string.  May not be null.  If a name
-     * @param pkeyField the name of the primary key field
+     
      * conflict exists, use the last table to be added as the table for a given name.
+     * @param pkeyField the name of the primary key field
      */
     public void addTable(DbFile file, String name, String pkeyField) {
         // some code goes here
+        if(name == null){
+          Debug.log("!!!warning: a table name of 'null' is founded.(Catalog.addTable)");
+          name = String.valueOf(file.getId());
+        }
+        Catalog.CatalogHeap cah = Catalog.new CatalogHeap(file,
+                                  name, pkeyField, file.getTupleDesc());  
+        catalog.put(Integer.valueOf(file.getId())); 
     }
 
     public void addTable(DbFile file, String name) {
@@ -77,7 +169,8 @@ public class Catalog {
      */
     public int getTableId(String name) throws NoSuchElementException {
         // some code goes here
-        return 0;
+        return 1; 
+ 	 
     }
 
     /**
@@ -88,7 +181,12 @@ public class Catalog {
      */
     public TupleDesc getTupleDesc(int tableid) throws NoSuchElementException {
         // some code goes here
-        return null;
+       Catalog.CatalogHeap cah= (Catalog.CatalogHeap) catalog.get(Integer.valueOf(tableid))
+       if(cah == null){
+         throw NoSuchElementException("no such table in the system catalog. The tableid is: " + String.valueOf(tableid));
+       } else{
+         return cah.tds;
+       } 
     }
 
     /**
@@ -99,12 +197,25 @@ public class Catalog {
      */
     public DbFile getDbFile(int tableid) throws NoSuchElementException {
         // some code goes here
-        return null;
+        //return null;
+        Catalog.CatalogHeap cah= (Catalog.CatalogHeap) catalog.get(Integer.valueOf(tableid))
+       if(cah == null){
+         throw NoSuchElementException("no such table in the system catalog. The tableid is: " + String.valueOf(tableid));
+       } else{
+         return cah.dbf;
+       }
     }
 
     public String getPrimaryKey(int tableid) {
         // some code goes here
-        return null;
+        // return null;
+        Catalog.CatalogHeap cah= (Catalog.CatalogHeap) catalog.get(Integer.valueOf(tableid))
+       if(cah == null){
+         throw NoSuchElementException("no such table in the system catalog. The tableid is: " + String.valueOf(tableid));
+       } else{
+         return cah.pkey;
+       }
+
     }
 
     public Iterator<Integer> tableIdIterator() {
@@ -114,16 +225,37 @@ public class Catalog {
 
     public String getTableName(int id) {
         // some code goes here
-        return null;
+        // return null;
+        Catalog.CatalogHeap cah= (Catalog.CatalogHeap) catalog.get(Integer.valueOf(tableid))
+       if(cah == null){
+         throw NoSuchElementException("no such table in the system catalog. The tableid is: " + String.valueOf(tableid));
+       } else{
+         return cah.entryName;
+       }
+
+
     }
     
     /** Delete all tables from the catalog */
     public void clear() {
         // some code goes here
+        System.out.println("----->Clearing the system catalog...");
+        catalog.clear();
+        System.out.println("----->System catalog clearing is done.");
     }
     
     /**
      * Reads the schema from a file and creates the appropriate tables in the database.
+     * The format of a catalogFile looks like this:
+     * 		TableName1(type field1 pk, type field2, type field3, ...)
+     *          TableName2(type field1 pk, type field2, type field3, ...)
+     *          TableName3(type field1 pk, type field2, type field3, ...)
+     *          TableName4(type field1 pk, type field2, type field3, ...)
+     *			.
+     *			.
+     *			.
+     *          TableNameN(type field1 pk, type field2, type field3, ...)
+     *
      * @param catalogFile
      */
     public void loadSchema(String catalogFile) {
